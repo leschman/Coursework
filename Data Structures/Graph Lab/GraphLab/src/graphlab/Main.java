b70455031c03266e3017f67e5f1fc035539f4d7c
Logan Esch <lesch@live.com>
2014-03-15 16:54:43 -0600

    got file reading working, need to interpret input to build graph.

--- Data Structures/Graph Lab/GraphLab/src/graphlab/Main.java
+++ Data Structures/Graph Lab/GraphLab/src/graphlab/Main.java
@@ -1,10 +1,9 @@
 package graphlab;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
 
 /**
  *
@@ -16,18 +15,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            try (Scanner s = new Scanner(new FileReader("C:/Users/Administrator/Documents/School/Data Structures/Graph Lab/GraphLab/src/graphlab/graph.txt"))) {
-                while (s.hasNextInt()) {
-                    System.out.print(s.nextInt());
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Administrator/Documents/School/Data Structures/Graph Lab/GraphLab/src/graphlab/graph.txt"))) {
 
+            String line = br.readLine();
+
+            while (line != null) {
+
+                String[] tokens = line.split(" ");
+                for (String t : tokens) {
+                    int i = Integer.parseInt(t);
+                    System.out.print(i + " ");
                 }
+                System.out.println();
+                line = br.readLine();
             }
 
         } catch (FileNotFoundException ex) {
             System.out.println("File not found.");
+        } catch (IOException ex) {
+            System.out.println("IO Exception.");
         }
-
     }
 
 }
