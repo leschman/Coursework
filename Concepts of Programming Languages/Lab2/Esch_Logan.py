28cbb87801b4c27657316a89574b1c7402af1eb1
Logan Esch <lesch@live.com>
2014-03-23 14:54:10 -0600

    built framework of game, need to build players.

--- Concepts of Programming Languages/Lab2/Esch_Logan.py
+++ Concepts of Programming Languages/Lab2/Esch_Logan.py
@@ -2,7 +2,6 @@
 Lab 2(Rock-Paper-Scissors-Lizard-Spock) Submission for Logan Esch.
 2014-03-24
 """
-#global variables
 
 
 #Super-class for the possible moves in the game.
@@ -78,14 +77,152 @@ class Paper(Element):
 			return('Paper disproves Spock', 'Win')
 		else:
 			return('Unknown name:' + oName)
+
+#Scissors 
+class Scissors(Element):
+	"""
+	Compare to method for Scissors implementation. 
+	takes another element and compares it to this one. 
+	Returns two strings, one with what happened, i.e. 'Scissors cut Paper' 
+	and another with the result i.e. 'Win'
+	"""
+	def compareTo(self, otherElement):
+		#get easier to type reference.
+		oName = otherElement.name()
+		
+		if 'Rock' == oName:
+			return ('Rock crushes Scissors', 'Lose')
+		elif 'Paper' == oName:
+			return ('Scissors cut Paper', 'Win')
+		elif 'Scissors' == oName:
+			return ('Scissors equals Scissors', 'Tie')
+		elif 'Lizard' == oName:
+			return('Scissors decapitate Lizard', 'Win')
+		elif 'Spock' == oName:
+			return('Spock smashes Scissors', 'Loose')
+		else:
+			return('Unknown name:' + oName)
 			
+
+#Lizard 
+class Lizard(Element):
+	"""
+	Compare to method for Lizard implementation. 
+	takes another element and compares it to this one. 
+	Returns two strings, one with what happened, i.e. 'Lizard poisons Spock' 
+	and another with the result i.e. 'Win'
+	"""
+	def compareTo(self, otherElement):
+		#get easier to type reference.
+		oName = otherElement.name()
+		
+		if 'Rock' == oName:
+			return ('Rock crushes Lizard', 'Lose')
+		elif 'Paper' == oName:
+			return ('Lizard eats Paper', 'Win')
+		elif 'Scissors' == oName:
+			return ('Scissors decapitate Lizard', 'Lose')
+		elif 'Lizard' == oName:
+			return('Lizard equals Lizard', 'Tie')
+		elif 'Spock' == oName:
+			return('Lizard poisons Spock', 'Win')
+		else:
+			return('Unknown name:' + oName)
+#Spock 
+class Spock(Element):
+	"""
+	Compare to method for Spock implementation. 
+	takes another element and compares it to this one. 
+	Returns two strings, one with what happened, i.e. 'Spock smashes Scissors' 
+	and another with the result i.e. 'Win'
+	"""
+	def compareTo(self, otherElement):
+		#get easier to type reference.
+		oName = otherElement.name()
+		
+		if 'Rock' == oName:
+			return ('Spock vaporizes Rock', 'Win')
+		elif 'Paper' == oName:
+			return ('Paper disproves Spock', 'Lose')
+		elif 'Scissors' == oName:
+			return ('Spock smashes Scissors', 'Win')
+		elif 'Lizard' == oName:
+			return('Lizard poisions Spock', 'Lose')
+		elif 'Spock' == oName:
+			return('Spock equals Spock', 'Tie')
+		else:
+			return('Unknown name:' + oName)
+
+#global variables
+
+
 
 rock = Rock('Rock')
 paper = Paper('Paper')
+scissors = Scissors('Scissors')
+lizard = Lizard('Lizard')
+spock = Spock('Spock')
+
+#the moves available in the game.
+moves = [rock, paper, scissors, lizard, spock]
+
+for move1 in moves:
+        print(move1.name())
+        for move2 in moves:
+                print(move1.compareTo(move2))
+
+
+
+"""
+
+moves = []
+moves.append(rock)
+moves.append(paper)
+moves.append(scissors)
+moves.append(lizard)
+moves.append(spock)
+
+
+print('Rock')
+print(moves[0].compareTo(moves[0]))
 print(rock.compareTo(paper))
+print(rock.compareTo(scissors))
+print(rock.compareTo(lizard))
+print(rock.compareTo(spock))
+
+
+print('Paper')
 print(paper.compareTo(rock))
-print(rock.compareTo(rock))
+print(paper.compareTo(scissors))
 print(paper.compareTo(paper))
+print(paper.compareTo(lizard))
+print(paper.compareTo(spock))
+
+print('Scissors')
+print(scissors.compareTo(rock))
+print(scissors.compareTo(paper))
+print(scissors.compareTo(scissors))
+print(scissors.compareTo(lizard))
+print(scissors.compareTo(spock))
+
+print('Lizard')
+print(lizard.compareTo(rock))
+print(lizard.compareTo(paper))
+print(lizard.compareTo(scissors))
+print(lizard.compareTo(lizard))
+print(lizard.compareTo(spock))
+
+
+print('Spock')
+print(spock.compareTo(rock))
+print(spock.compareTo(paper))
+print(spock.compareTo(scissors))
+print(spock.compareTo(lizard))
+print(spock.compareTo(spock))
+"""
+
+
+
 			
 			
 			
