887a2affff5a08d91ef5c48cdb5c60cb37941d8d
Logan Esch <lesch@live.com>
2014-03-23 14:06:35 -0600

    debuged Element, Rock and Paper. need to build the rest.

--- Concepts of Programming Languages/Lab2/Esch_Logan.py
+++ Concepts of Programming Languages/Lab2/Esch_Logan.py
@@ -7,7 +7,7 @@ Lab 2(Rock-Paper-Scissors-Lizard-Spock) Submission for Logan Esch.
 
 #Super-class for the possible moves in the game.
 class Element:
-	_name
+	_name = ''
 	"""
 	constructor for Element Class
 	takes a string and initializes the name variable of the class to that string.
@@ -18,39 +18,40 @@ class Element:
 	"""
 	Getter method for name variable
 	"""
-	def name():
-		return _name
+	def name(self):
+		return self._name
 	
 	"""
 	Compare to method stub, needs to be implemented in client classes.
 	"""
-	def compareTo()
+	def compareTo():
 		raise NotImplementedError("Not yet Implemented")
 
 		
 #Rock 
 class Rock(Element):
+
 	"""
 	Compare to method for Rock implementation. 
 	takes another element and compares it to this one. 
 	Returns two strings, one with what happened, i.e. 'Rock crushes Scissors' 
 	and another with the result i.e. 'Win'
 	"""
-	def compareTo(otherElement):
+	def compareTo(self, otherElement):
 		#get easier to type reference.
-		oName = otherElement.super.name()
+		oName = otherElement.name()
 		
-		if 'Rock' = oName:
+		if 'Rock' == oName:
 			return ('Rock equals Rock', 'Tie')
-		elif 'Paper' = oName:
-			return ('Paper covers Rock', 'Loose')
-		elif 'Scissors' = oName:
+		elif 'Paper' == oName:
+			return ('Paper covers Rock', 'Lose')
+		elif 'Scissors' == oName:
 			return ('Rock crushes Scissors', 'Win')
-		elif 'Lizard' = oName:
+		elif 'Lizard' == oName:
 			return('Rock crushes Lizard', 'Win')
-		elif 'Spock' = oName:
-			return('Spock vaporizes Rock', 'Loose')
-		else
+		elif 'Spock' == oName:
+			return('Spock vaporizes Rock', 'Lose')
+		else:
 			return('Unknown name:' + oName)
 
 #Paper 
@@ -61,24 +62,30 @@ class Paper(Element):
 	Returns two strings, one with what happened, i.e. 'Paper covers Rock' 
 	and another with the result i.e. 'Win'
 	"""
-	def compareTo(otherElement):
+	def compareTo(self, otherElement):
 		#get easier to type reference.
-		oName = otherElement.super.name()
+		oName = otherElement.name()
 		
-		if 'Rock' = oName:
+		if 'Rock' == oName:
 			return ('Paper covers Rock', 'Win')
-		elif 'Paper' = oName:
+		elif 'Paper' == oName:
 			return ('Paper equals Paper', 'Tie')
-		elif 'Scissors' = oName:
-			return ('Scissors cut Paper', 'Loose')
-		elif 'Lizard' = oName:
-			return('Lizard eats Paper', 'Loose')
-		elif 'Spock' = oName:
+		elif 'Scissors' == oName:
+			return ('Scissors cut Paper', 'Lose')
+		elif 'Lizard' == oName:
+			return('Lizard eats Paper', 'Lose')
+		elif 'Spock' == oName:
 			return('Paper disproves Spock', 'Win')
-		else
+		else:
 			return('Unknown name:' + oName)
 			
-			
+
+rock = Rock('Rock')
+paper = Paper('Paper')
+print(rock.compareTo(paper))
+print(paper.compareTo(rock))
+print(rock.compareTo(rock))
+print(paper.compareTo(paper))
 			
 			
 			
