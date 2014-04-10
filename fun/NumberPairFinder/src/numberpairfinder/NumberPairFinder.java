88481a40920cacc63cc6eef034d765c611cf2af5
Logan Esch <lesch@live.com>
2014-04-10 09:29:11 -0600

    added just for fun projects.

--- fun/NumberPairFinder/src/numberpairfinder/NumberPairFinder.java
+++ fun/NumberPairFinder/src/numberpairfinder/NumberPairFinder.java
@@ -25,47 +25,42 @@ public class NumberPairFinder implements Runnable {
     int[] test;
     int val;
     long runningTime = 0;
+    static ArrayList<String> results = new ArrayList<>();
 
     public NumberPairFinder(int problemSize, int range, int val) {
         this.problemSize = problemSize;
         this.range = range;
         this.val = val;
         test = new int[problemSize];
-
+        this.generateRandomNumbers();
+    }
+    
+    public void generateRandomNumbers(){
         for (int i = 0; i < problemSize; i++) {
             test[i] = r.nextInt(range - 1) + 1;
         }
-
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        File f = new File("benchmark.csv");
-        try {
-            if (!f.exists()) {
-                f.createNewFile();
+        int maximumSize = 4000000;
+        int iterationSize = 1000000;
+        for (int val = iterationSize; val <= maximumSize; val += iterationSize) {
+            
+            NumberPairFinder n = new NumberPairFinder(val, val, val);
+            Thread t = new Thread(n);
+            
+            for (int i = 0; i < 10; i++) {
+//                NumberPairFinder n = new NumberPairFinder(val, val, val);
+//                n.run();      
+                
+                t.start();
+                n.generateRandomNumbers();
             }
-            FileWriter fw = new FileWriter(f.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            String out = "Problem Size,Range,RunningTime,Trial\n";
-            bw.write(out);
-            for (int val = 1000000; val <= 2000000; val += 1000000) {
-
-                for (int i = 0; i < 10; i++) {
-
-                    NumberPairFinder n = new NumberPairFinder(val, val, val);
-                    n.run();
-                    out = val + "," + val + "," + n.runningTime + "," + i + "\n";
-                    bw.write(out);
-                }
-            }
-            bw.close();
-        } catch (IOException ex) {
-            Logger.getLogger(NumberPairFinder.class.getName()).log(Level.SEVERE, null, ex);
         }
+        NumberPairFinder.printResultsToFile();
     }
 
     public Point[] hashNumberPairFinder(int[] nums, int val) {
@@ -117,10 +112,29 @@ public class NumberPairFinder implements Runnable {
         ThreadMXBean bean = ManagementFactory.getThreadMXBean();
 
         long startTime = bean.getCurrentThreadCpuTime();
-        Point[] solution = hashNumberPairFinder(test, val);
+        hashNumberPairFinder(test, val);
         long endTime = bean.getCurrentThreadCpuTime();
 
         //System.out.println("Problem Size: " + problemSize + " Range: " + range);
-        runningTime = endTime - startTime;
+
+        results.add(val + "," + val + "," + (endTime - startTime) + "\n");
+    }
+
+    public static void printResultsToFile() {
+        File f = new File("benchmark.csv");
+        try {
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            FileWriter fw = new FileWriter(f.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Problem Size,Range,RunningTime,Trial\n");
+            for (String out : results) {
+                bw.write(out);
+            }
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NumberPairFinder.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
