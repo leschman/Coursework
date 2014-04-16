27bd49b6c3e3e612aaf76cb180cd50e9a1e54947
Logan Esch <lesch@live.com>
2014-04-16 17:15:05 -0600

    implemented data memory

--- Logic Design/Final Project/rw_96x8_sync.vhd
+++ Logic Design/Final Project/rw_96x8_sync.vhd
@@ -28,12 +28,41 @@ entity rw_96x8_sync is
           data_in  : in  std_logic_vector(7 downto 0);    
           write    : in  std_logic;
           address  : in  std_logic_vector(7 downto 0);
-          data_out : out std_logic_vector(7 downto 0));
+          data_out  : out std_logic_vector(7 downto 0));
 end entity;
 
 
 architecture rw_96x8_sync_arch of rw_96x8_sync is
 
- -- add functionality here...
+ --Signal Declaration
+ type RW_type is array (0 to 96) of std_logic_vector(3 downto 0);
+ signal RW 	: RW_type;
+ signal EN	: std_logic;
+ 
+ begin
+	ENABLE : process (address)
+	begin
+		if((to_integer(unsigned(address)) >= 128) and (to_integer(unsigned (address)) <= 223)) then
+			EN <= '1';
+		else
+			EN <= '0';
+		end if;
+	end process;
+ 
+ 
+ 
+	MEMORY : process(clock)
+	begin
+		if(clock'event and clock = '1') then
+			if(EN = '1')
+				--check if we are writing or reading
+				if( write = '1') then 
+					RW(to_integer(unsigned(address))) <= data in;
+				else
+					data_out <= RW(to_integer(unsigned(address)));
+				end if;
+			end if;
+		end if;
+	end process;		
 
 end architecture;   
