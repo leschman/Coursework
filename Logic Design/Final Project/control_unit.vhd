0c57e605b3a62235a20ea53b44ae5460ba29ddd8
Logan Esch <lesch@live.com>
2014-04-18 16:18:49 -0600

    built control and processing units. need to test. implemented LDA, STA_DIR, and BRA instructions

--- Logic Design/Final Project/control_unit.vhd
+++ Logic Design/Final Project/control_unit.vhd
@@ -5,9 +5,9 @@
 --               
 -- Description : VHDL model of the CPU control finite state machine
 --
--- Author(s)   : 
+-- Author(s)   : Logan Esch
 --
--- Date        : 
+-- Date        : April 18, 2014
 --
 ------------------------------------------------------------------------------------------------------------
 library IEEE;
@@ -32,9 +32,171 @@ entity control_unit is
 end entity;
 
 architecture control_unit_arch of control_unit is
-  
+
+-- Constants for Instruction Pnemonics
+	constant LDA_IMM  : std_logic_vector (7 downto 0) := x"86";   -- Load Register A with Immediate Addressing
+	constant LDA_DIR  : std_logic_vector (7 downto 0) := x"87";   -- Load Register A with Direct Addressing
+	constant STA_DIR  : std_logic_vector (7 downto 0) := x"96";   -- Store Register A to memory (RAM or IO)
+	constant LDB_IMM  : std_logic_vector (7 downto 0) := x"88";   -- Load Register B with Immediate Addressing
+	constant LDB_DIR  : std_logic_vector (7 downto 0) := x"89";   -- Load Register B with Direct Addressing
+	constant STB_DIR  : std_logic_vector (7 downto 0) := x"97";   -- Store Register B to memory (RAM or IO)
+	constant BRA      : std_logic_vector (7 downto 0) := x"20";   -- Branch Always                           
+	constant BEQ      : std_logic_vector (7 downto 0) := x"21";   -- Branch if Equal to Zero
+	constant ADD_AB   : std_logic_vector (7 downto 0) := x"42";   -- A <= A + B
+	constant SUB_AB   : std_logic_vector (7 downto 0) := x"43";   -- A <= A - B
+	constant AND_AB   : std_logic_vector (7 downto 0) := x"44";   -- A <= A and B
+	constant OR_AB    : std_logic_vector (7 downto 0) := x"45";   -- A <= A or B
+	constant INCA     : std_logic_vector (7 downto 0) := x"46";   -- A <= A + 1
+	constant INCB     : std_logic_vector (7 downto 0) := x"47";   -- B <= B + 1
+	constant DECA     : std_logic_vector (7 downto 0) := x"48";   -- A <= A - 1
+	constant DECB     : std_logic_vector (7 downto 0) := x"49";   -- B <= B - 1
+	
+--Constants for Processing Unit MUX control
+	constant PC		: std_logic_vector(1 downto 0)	:= "00";
+	constant A		: std_logic_vector(1 downto 0)	:= "01";
+	constant B		: std_logic_vector(1 downto 0)	:= "10";
+	
+	constant ALU	: std_logic_vector(1 downto 0)	:= "00";
+	constant BUS1	: std_logic_vector(1 downto 0)	:= "01";
+	constant MEM	: std_logic_vector(1 downto 0)	:= "10";
+	
+
+	type state is(	S_FETCH_0, 		S_FETCH_1, 		S_FETCH_2, 		S_DECODE_3, 
+					S_LDA_IMM_4, 	S_LDA_IMM_5,	S_LDA_IMM_6,
+					S_STA_DIR_4,	S_STA_DIR_5,	S_STA_DIR_6,	S_DIR_7,
+					S_BRA_4,		S_BRA_5,		S_BRA_6);
+	signal currentState, nextState : state;
+
 begin
-  
- -- add functionality here...
+	CLOCK : process(clock, reset)
+	begin
+		if(reset = '0')then
+			currentState <= S_FETCH_0;
+			nextState <= S_FETCH_1;
+		elsif(clock'event and clock = '1')then
+			currentState <= nextState;
+		else
+			currentState <= currentState;
+		end if;
+	end process;
+
+	UPDATE_STATE : process(currentState)
+	begin
+		case state is
+------------------------------------------------------------------------------------------------
+--								 FETCH
+------------------------------------------------------------------------------------------------
+		--Place the PC Value into the MAR in order to provide the address for the OpCode.
+		when S_FETCH_0 => 
+			A_Load <= '0';
+			write <= '0';
+			pc_load <= '0';
+			bus1_sel <= PC;
+			bus2_sel <= BUS1;
+			MAR_Load <= '1';
+			nextState <= S_FETCH_1;
+			
+		--Put the OpCode arriving on From_Memory on Bus2, Increment PC_Inc.
+		when S_FETCH_1 =>
+			MAR_Load <= '0';
+			bus2_sel <= MEM;
+			PC_Inc <= '1';
+			nextState <= S_FETCH_1;
+			
+		--Latch the OpCode from Bus2 into IR.
+		when S_FETCH_2 =>
+			PC_Inc <= '0';
+			IR_Load <= '1';
+			nextState <= S_DECODE_3;
+		
+		--Decode the OpCode and determine the next sequence.
+		when S_DECODE_3 =>
+			IR_Load <= '0';
+			case IR is
+			when LDA_IMM 	=> nextState <= S_LDA_IMM_4;
+			when STA_DIR 	=> nextState <= S_STA_DIR_4;
+			when BRA 		=> nextState <= S_BRA_4;
+			when others 	=> nextState <= S_FETCH_0;
+			end case;
+------------------------------------------------------------------------------------------------
+--								 LDA
+------------------------------------------------------------------------------------------------		
+		--PC is pointing at address to load, move PC to MAR
+		when S_LDA_IMM_4 =>
+			Bus1_Sel <= PC;
+			Bus2_Sel <= bus1;
+			MAR_Load <= '1';
+			nextState <= S_LDA_IMM_5;
+			
+		--Move the Operand on From_Memory onto Bus2, Increment PC_Inc.
+		when S_LDA_IMM_5 =>
+			MAR_Load <= '0';
+			Bus2_sel <= from_memory;
+			pc_inc <= '1';
+			nextState <= S_LDA_IMM_6;
+		
+		--Latch the Operand from Bus2 into A.
+		when S_LDA_IMM_6 =>
+			pc_inc <= '0';
+			A_Load <= '1';
+			nextState <= S_FETCH_0;
+------------------------------------------------------------------------------------------------
+--								 STA_DIR
+------------------------------------------------------------------------------------------------			
+		--PC is pointing to the address we are moving A to. Move PC to MAR. 
+		when S_STA_DIR_4 =>
+			BUS1_SEL <= PC;
+			Bus2_sel <= BUS1;
+			MAR_Load <= '1';
+			nextState <= S_STA_DIR_5;
+		
+		--Move From_Memory onto Bus2, Increment PC_Inc.
+		when S_STA_DIR_5 =>
+			MAR_Load <= '0';
+			Bus2_sel <= from_memory;
+			PC_Inc <= '1';
+			nextState <= S_STA_DIR_6;
+		
+		--Address we are writing to is on Bus2, latch into MAR.
+		when S_STA_DIR_6 =>
+			PC_Inc <= '0';
+			MAR_Load <= '1';
+			nextState <= S_STA_DIR_7;
+		
+		--MAR is pointing to the Write Address, Move A to Bus1, assert Write.
+		when S_STA_DIR_7 =>
+			MAR_Load <= '0';
+			Bus1_Sel <= A;
+			write <= '1';
+			nextState <= S_FETCH_0;
+------------------------------------------------------------------------------------------------
+--								 BRA
+------------------------------------------------------------------------------------------------		
+		--PC is pointing to operand we are jumping to. Move PC to MAR.
+		when S_BRA_4 =>
+			bus1_sel <= PC;
+			bus2_sel <= bus1;
+			mar_load <= '1';
+			nextState <= S_Bra_5;
+		
+		--Move operand onto Bus2 from From_Memory.
+		when S_BRA_5 =>
+			mar_load <= '0';
+			bus2_sel <= from_memory;
+			nextState <= S_Bra_6;
+			
+		--Load Bus2 into PC.
+		when S_BRA_6 =>
+			PC_Load <= '1';
+			nextState <= S_Fetch_0;
+		
+------------------------------------------------------------------------------------------------
+--								 OTHERS
+------------------------------------------------------------------------------------------------		
+		when others => nextState <= S_FETCH_0;
+			
+			
+			
+
 
 end architecture;
--- Logic Design/Final Project/processing_unit.vhd
+++ Logic Design/Final Project/processing_unit.vhd
@@ -5,9 +5,9 @@
 --               
 -- Description : VHDL model of the CPU processing unit including
 --
--- Author(s)   : 
+-- Author(s)   : Logan Esch	
 --
--- Date        : 
+-- Date        : April 18, 2014
 --
 ------------------------------------------------------------------------------------------------------------
 library IEEE;
@@ -36,6 +36,126 @@ end entity;
 
 architecture processing_unit_arch of processing_unit is
 begin
- -- add functionality here...
+--Component Declaration
+	--component alu
+	--port();
 
+	--component ccr
+	--port();
+
+--Signal Declaration
+	signal bus1 	std_logic_vector(7 downto 0):= "00000000";
+	signal bus2 	std_logic_vector(7 downto 0):= "00000000";
+	signal IRR 		std_logic_vector(7 downto 0):= "00000000";
+	signal MAR		std_logic_vector(7 downto 0):= "00000000";
+	signal PC		std_logic_vector(7 downto 0):= "00000000";
+	signal A 		std_logic_vector(7 downto 0):= "00000000";
+	signal B 		std_logic_vector(7 downto 0):= "00000000";
+	
+	
+begin
+
+--Component Instantiation
+
+--Processes
+	INSTRUCTION_REGISTER : process(clock)
+	begin
+		if(reset = '0')then
+			IRR <= "00000000";
+			IR <= IRR;
+		elsif(clock'event and clock = '1') then
+			--check if IR_Load is enabled
+			if(IR_Load = '1')then
+				IRR <= bus2;
+				IR <= IRR;
+			end if;
+		end if;
+	end process;
+	
+	MEMORY_ADDRESS_REGISTER : process(clock)
+	begin
+		if(reset = '0')then
+			MAR <= "00000000";
+			address <= MAR;
+		elsif(clock'event and clock = '1') then
+			--check if MAR_Load is enabled
+			if(MAR_Load = '1')then
+				MAR <= bus2;
+				address <= MAR;
+			end if;
+		end if;
+	end process;
+	
+	PROGRAM_COUNTER : process(clock)
+	begin	
+		if(reset = '0')then
+			PC <= "00000000";
+		elsif(clock'event and clock = '1') then
+			--check if PC_LOAD is enabled
+			if(PC_Load = '1')then
+				PC <= bus2;
+				
+			--check if PC_Inc is enabled
+			elsif(PC_Inc = '1')then
+				--increment the PC
+				PC <= std_logic_vector(to_unsigned(to_integer(unsigned(PC)) + 1) ,8);
+			end if;
+		end if;
+	end process;
+	
+	REGISTER_A : process(clock)
+	begin
+		if(reset = '0')then
+			A <= "00000000";
+		elsif(clock'event and  clock ='1')then
+			--check if A_Load is enabled
+			if(A_Load = '1') then
+				A <= bus2;
+			end if;
+		end if;
+	end process;
+	
+	REGISTER_B : process(clock)
+	begin
+		if(reset = '0')then
+			B <= "00000000";
+		elsif(clock'event and  clock ='1')then
+			--check if B_Load is enabled
+			if(B_Load = '1') then
+				B <= bus2;
+			end if;
+		end if;
+	end process;
+	
+	BUS1_MUX : process(clock)
+	begin
+		if(reset = '0')then
+			Bus1 <= "00000000";
+		elsif(clock'event and clock = '1')then
+			case bus1_sel is
+			when "00" => bus1 <= pc;
+			when "01" => bus1 <= A;
+			when "10" => bus1 <= B;
+			when others => bus1 <= "00000000";
+			end case;
+		end if;
+	end process;
+	
+	BUS2_MUX : process(clock)
+	begin
+		if(reset = '0')then
+			Bus2 <= "00000000";
+		elsif(clock'event and clock = '1')then
+			case bus2_sel is
+			--when "00" => bus2 <= ALU_out;
+			when "01" => bus2 <= bus1;
+			when "10" => bus2 <= from_memory;
+			when others => bus2 <= "00000000";
+			end case;
+		end if;
+	end process;
+
+	to_memory <= Bus1;
+
+	
 end architecture;
