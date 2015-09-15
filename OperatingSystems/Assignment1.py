8bcb0dfc5a489cb03081668ffdaaced7b17d3874
Logan Esch <lesch@live.com>
2015-09-15 17:55:56 -0600

    having issues with rr scheduler.

--- OperatingSystems/Assignment1.py
+++ OperatingSystems/Assignment1.py
@@ -27,6 +27,7 @@ class Core:
 			if (self.job.worked_on_time == self.job.processing_time):
 				#job is done, mark it as such and delete it. 
 				self.job.finish_time = time
+				print("Core {0} finished job {1} at time {2}.".format(self.number, self.job.number, time))
 				self.job = None
 
 		else: #if the core doesn't have a job it's working on.
@@ -35,10 +36,12 @@ class Core:
 				# there is a job, load it and wait for the next cycle (the 1ms penelty).
 				self.job = self.queue.popleft()
 				self.job.begin_time = time
+				print("Core {0} begining job {1} at time {2}.".format(self.number, self.job.number, time))
 				self.busy = True
 			else:
 				# there is no job to perform next, so idle. 
 				self.busy = False
+				print("Core {0} is idle at time {1}.".format(self.number, time))
 
 class Job:
 	"""
@@ -61,12 +64,14 @@ class Processor:
 		self.clock = 0 # system clock, represented by an int, each 'tick' will be 1ms in simulation-land. 
 		self.jobset = jobset
 		self.next_job_index = 0
+		self.scheduler = None
+		self.all_scheduled = False
 
 		# Core Initlization
-		core_count = (1303 % 3) + 2 #student id mod 3, plus 2
+		self.core_count = (1303 % 3) + 2 #student id mod 3, plus 2
 		self.cores = [] #declare a list of cores.
 		#loop through and initalize all cores
-		for number in range(0, core_count - 1): 
+		for number in range(0, self.core_count): 
 			self.cores.append(Core(number))
 
 		#job buffer for new jobs as they come in
@@ -82,16 +87,16 @@ class Processor:
 		self.busy = False
 
 		#pull in new jobs from the jobset.
-		if next_job_index < len(jobset):
-			#pull the next job off the jobset (ASSUMES JOBS ORDERED BY ARIVAL TIME)
-			next_job = self.jobset[self.next_job_index]
-			# if it's time to start the next job, put the job in the job queue (like it arived)
-			if next_job.arrival_time == time:
-				self.jobs.append(next_job)
-				self.next_job_index += 1 # update the index
+		if not self.all_scheduled:
+			while self.jobset[self.next_job_index].arrival_time <= self.clock:
+				self.jobs.append(self.jobset[self.next_job_index])
+				self.next_job_index += 1
+				if self.next_job_index >= (len(self.jobset) -1):
+					self.all_scheduled = True
+					break
 
 		# schedule the arived jobs.
-		scheduler.schedule()
+		self.scheduler.schedule()
 
 		# have the cores do 'work'. 
 		for core in self.cores:
@@ -101,26 +106,27 @@ class Processor:
 			if core.busy:
 				self.busy = True
 
+
 class RoundRobinScheduler:
 	"""
 	Round robin job scheduler, takes jobs and puts them imediatly on the next core in a circular manner.
 	"""
 
-	next_core = 0 # counter for the next core to schedule a job on. 
-
 	def __init__(self, processor):
 		self.processor = processor
+		self.next_core = 0 # counter for the next core to schedule a job on.
 
-	def schedule():
+	def schedule(self):
 		#if the processor has jobs to be scheduled, put one into the core job queues. 
-		if processor.jobs:
-			processor.cores[next_core].queue.append(processor.jobs.popleft()) #enqueue the job on the next core
+		if self.processor.jobs:
+	
+			self.processor.cores[self.next_core].queue.append(self.processor.jobs.popleft()) #enqueue the job on the next core
 
 			# increment the next core to schedule. 
-			next_core += 1
-			if next_core >= processor.core_count:
+			self.next_core += 1
+			if self.next_core >= self.processor.core_count:
 				# if the counter is too high, reset it.  
-				next_core = 0
+				self.next_core = 0
 		else: 
 			pass
 
@@ -130,13 +136,13 @@ class ShortestJobNextScheduler:
 		self.processor = processor
 		self.priority_queue = PriorityQueue()
 
-	def schedule():
+	def schedule(self):
 		#check if there are new jobs in the processors buffer, if so prioritize them. 
-		while processor.jobs:
-			self.priority_queue.put(processor.jobs.popleft())
+		while self.processor.jobs:
+			self.priority_queue.put(self.processor.jobs.popleft())
 
 		#check if the cores need more jobs. 
-		for core in processor.cores:
+		for core in self.processor.cores:
 			if not core.jobs:
 				core.jobs.append(self.priority_queue.get())
 
@@ -160,12 +166,14 @@ def main():
 		jobset = default_jobset()
 
 	#initialize the processor.
-	processor = Processor(jobset)	
+	processor = Processor(jobset)
+	scheduler = RoundRobinScheduler(processor)	
 
 	if args.scheduler == 'sjn':
 		scheduler = ShortestJobNextScheduler(processor)
-	elif args.scheduler == 'rr':
-		scheduler = RoundRobinScheduler(processor)
+
+
+	processor.scheduler = scheduler
 
 	idle_count = 0
 	while idle_count < 10:
@@ -200,7 +208,7 @@ def main():
 
 	average = summation / count
 
-	print("Min: {0}, Max: {1}, Avg: {2}, Std Dev: ", minimum, maximum, average)
+	print("Min: {0}, Max: {1}, Avg: {2}, Std Dev: ".format(minimum, maximum, average))
 
 
 
@@ -219,18 +227,22 @@ def default_jobset():
 	""" builds and returns the default jobset given in the assignment."""
 	jobset = []
 
-	jobset.append(Job(1,4,9))
-	jobset.append(Job(2,15,2))
-	jobset.append(Job(3,18,16))
-	jobset.append(Job(4,20,3))
-	jobset.append(Job(5,26,29))
-	jobset.append(Job(6,29,198))
-	jobset.append(Job(7,35,7))
-	jobset.append(Job(8,45,170))
-	jobset.append(Job(9,57,180))
-	jobset.append(Job(10,83,178))
-	jobset.append(Job(11,88,73))
-	jobset.append(Job(12,95,8))
+	jobset.append(Job(1,1,5))
+	jobset.append(Job(2,1,5))
+	jobset.append(Job(3,1,5))
+
+	#jobset.append(Job(1,4,9))
+	#jobset.append(Job(2,15,2))
+	#jobset.append(Job(3,18,16))
+	#jobset.append(Job(4,20,3))
+	#jobset.append(Job(5,26,29))
+	#jobset.append(Job(6,29,198))
+	#jobset.append(Job(7,35,7))
+	#jobset.append(Job(8,45,170))
+	#jobset.append(Job(9,57,180))
+	#jobset.append(Job(10,83,178))
+	#jobset.append(Job(11,88,73))
+	#jobset.append(Job(12,95,8))
 
 	return jobset
 
